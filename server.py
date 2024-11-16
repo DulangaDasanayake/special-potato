a7018ba518cca015ddd377f69e622a9fa05a2a16
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-11-16 23:56:42 +0530

    Start the server

--- server.py
+++ server.py
@@ -21,3 +21,15 @@ def broadcast(msg, sender_socket):
     for client in clients:
         if client != sender_socket:
             client.send(msg.encode())
+
+# Start the server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen(5)
+print(f"Server started on {HOST}:{PORT}")
+
+while True:
+    client_socket, addr = server.accept()
+    print(f"Connection from {addr}")
+    clients.append(client_socket)
+    threading.Thread(target=handle_client, args=(client_socket,)).start()
